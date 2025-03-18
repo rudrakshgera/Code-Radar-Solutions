7e5db597d69475510b67a186441bfe01464d0877
rudrakshgera <rudrakshgera@gmail.com>
2025-03-18 21:37:41 +0530

    Solution to Check Triangle Validity

---
+++ Check Triangle Validity@@6788dd63490637cfc44e1b93/code.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    printf("Valid")
+    // int x;
+    // scanf("%d", &x);
+    // if (x%5==0 && x%11==0) {
+    //     printf("Divisible");
+    // }
+    // else {
+    //     printf("Not Divisible");
+    // }
+  return 0;
+}
